ea14ee6ff9bac63c2a442fa23e3956c24e09bf87
Henrique Faria <hd.faria@poli.ufrj.br>
2019-08-16 15:30:45 -0300

    code1: command pin13 via serial port

---
+++ code1/code1.ino
@@ -0,0 +1,30 @@
+// Simple piece of code to read serial 
+// port and turn LED/Pin13 On or Off:
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read from port 0:
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    switch (inByte)
+    {
+    case 'l':
+      // Turn LED/Pin13 On:
+      digitalWrite(LED_BUILTIN, HIGH);
+      break;
+    case 'd':
+      // Turn LED/Pin13 Off:
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+    
+    default:
+      //Do nothing
+      break;
+    }
+  
+  }
+}
