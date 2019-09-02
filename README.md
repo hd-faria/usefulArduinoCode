4cda2be930bf9c450c217116a2647cdb254ee969
Henrique <hd.faria@poli.ufrj.br>
2019-09-02 17:37:03 -0300

    update state machines

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # usefulArduinoCode
 I'm storing here a bunch of arduino piece of code that i usually need
 
-"${workspaceFolder}/**"
--- code4/code4.ino
+++ code4/code4.ino
@@ -2,34 +2,37 @@
 // using function pointer to turn LED/Pin13 On or Off
 // adapted from: https://codeandlife.com/2013/10/06/tutorial-state-machines-with-c-callbacks/
 
+typedef void *(*StateFunc)();
+
 // States
-void led_on();
-void led_off();
+void *led_on();
+void *led_off();
 
 // State pointer
-void (*statefunc)() = led_on;
+StateFunc statefunc;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize serial port:
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  statefunc = led_on;
 }
 
 void loop() {
       
     if (Serial.available()) {
       int inByte = Serial.read();
-      (*statefunc)();
+      statefunc = (StateFunc)(*statefunc)();
     }  
 
 }
 
-void led_on(){
+void *led_on(){
   digitalWrite(LED_BUILTIN, HIGH);
-  statefunc = led_off;
+  return led_off;
 }
 
-void led_off(){
+void *led_off(){
   digitalWrite(LED_BUILTIN, LOW);
-  statefunc = led_on;
+  return led_on;
 }
