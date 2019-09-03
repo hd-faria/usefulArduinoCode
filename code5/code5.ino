09d5784cad1802e734f9cd09f712b5a7df792c55
Henrique <hd.faria@poli.ufrj.br>
2019-09-03 15:29:41 -0300

    another state machiene.

--- code5/code5.ino
+++ code5/code5.ino
@@ -1,11 +1,25 @@
-typedef void *(*StateFunc)();
+// A state Machiene impementation, besed on
+// https://codeandlife.com/2013/10/06/tutorial-state-machines-with-c-callbacks/
+// but using a struct to hold some values of interest. Sadly I still have to 
+// find a way of define de struct locally and pass it to the functions that represent 
+// transitions
+
 
 //State
-struct {
-    unsigned int is_ON : 1;
+struct state {
+    uint8_t       : 1;
+    uint8_t       : 1;  
+    uint8_t       : 1;
+    uint8_t       : 1;
+    uint8_t       : 1;
+    uint8_t       : 1;
+    uint8_t       : 1;
+    uint8_t       : 1;    
+    uint8_t is_ON : 1;
     /* data */
-};
+} systemState;
 
+typedef void *(*StateFunc)();
 
 // State transition
 void *led_on();
@@ -17,7 +31,8 @@ StateFunc statefunc;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // initialize serial port:
-  Serial.begin(9600);  
+  Serial.begin(9600);
+  systemState.is_ON=0;  
   statefunc = led_on;
 }
 
@@ -31,11 +46,17 @@ void loop() {
 }
 
 void *led_on(){
+  systemState.is_ON = 1;
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print("Estado: ");
+  Serial.println(systemState.is_ON);
   return led_off;
 }
 
 void *led_off(){
+  systemState.is_ON = 0;
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("Estado: ");
+  Serial.println(systemState.is_ON);
   return led_on;
 }
